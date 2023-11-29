e39efb0b2860a1be6f885c9040b933e5cf08c27c
masvel <maslov.pa@phystech.edu>
2023-11-29 10:39:42 +0300

    seems working

--- les4/cool_grid.cpp
+++ les4/cool_grid.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cassert>
 #include <memory>
+#include "./array.h"
 
 template<typename T, std::size_t N>
 class Grid final {
@@ -11,13 +12,11 @@ public:
 
 private:
     size_type m_size;
-    Grid<T, N-1> * m_data;
+    Array<Grid<T, N-1>> m_data;
 
 public:
     template<typename... Args>
-    Grid(size_type size, Args... subgrid_args):m_size(size), m_data(new Grid<T, N-1>[m_size]){
-        for (size_type i = 0; i < size; i++) { m_data[i] = Grid<T, N-1>(subgrid_args...); }
-    } ;
+    Grid(size_type size, Args... subgrid_args):m_size(size), m_data(m_size, Grid<T, N-1>(subgrid_args...)){} ;
 
     template<typename... Args>
     T operator()(size_type idx, Args... args) const {
@@ -26,13 +25,13 @@ public:
 
     Grid(): m_data(new Grid<T, N-1>()), m_size(1) {};
 
-    Grid<T, N-1>& operator[](size_type n) const {
+    const Grid<T, N-1>& operator[](size_type n) const {
         return m_data[n];
     }
 
     Grid(Grid<T, N> const& other)
-    : m_data(new Grid<T, N-1>[other.m_size])
-    , m_size(other.m_size) 
+    : m_size(other.m_size) 
+    , m_data(m_size, Grid<T, N-1>())
     { 
         for (size_type i = 0; i < m_size; i++) { m_data[i] = other.m_data[i]; }
     }
@@ -56,25 +55,21 @@ public:
     using size_type = std::size_t;
 
 private:
-    T* m_data;
     size_type m_size;
+    Array<T> m_data;
 
 public:
-    Grid(size_t size, T t) :m_data(new T[size]), m_size(size) {
-        for (size_type i = 0; i < m_size; i++) { m_data[i] = T(t); }
-    }
+    Grid(size_t size, T t) : m_size(size), m_data(size, t) {};
 
-    Grid(): m_data(new T[1]), m_size(1) {
-        m_data[0] = 0;
-    }
+    Grid(): m_size(1), m_data(1, 0) {}
 
     T operator()(size_type idx) const {
         return m_data[idx];
     }
 
     Grid(Grid<T, 1> const& other)
-    : m_data(new T[other.m_size])
-    , m_size(other.m_size) 
+    : m_size(other.m_size) 
+    , m_data(m_size, T())
     { 
         for (size_type i = 0; i < m_size; i++) { m_data[i] = other.m_data[i]; }
     }
@@ -86,9 +81,6 @@ public:
         return *this;
     }
 
-    ~Grid() {
-        delete[] m_data;
-    }
     
 
 };
